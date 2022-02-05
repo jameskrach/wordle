66170a172b799440db81a4aa92c927650a29af03
James Krach <2436093-james.krach@users.noreply.gitlab.com>
2022-02-04 22:38:13 -0500

    Improve benchmark performance

--- wordle/player.py
+++ wordle/player.py
@@ -2,6 +2,7 @@
 import re
 import random
 from abc import ABC, abstractmethod
+from concurrent.futures import Executor
 from pathlib import Path
 from string import ascii_lowercase
 
@@ -35,10 +36,12 @@ class BasePlayer(PlayerInterface):
     """ABC with a few methods that are likely useful to all player implementations."""
     def __init__(self, word_size: int = WORD_SIZE, max_guess: int = MAX_GUESS, word_file: Path = WORD_FILE) -> None:
         super().__init__(word_size=word_size, max_guess=max_guess, word_file=word_file)
+        self._cache_guesses: list[str] = []
+        self._cache_remaining_words: list[str] = self.all_words.copy()
 
     @property
-    def remaining_letters(self) -> str:
-        """Letters that have not been excluded by previous responses."""
+    def bad_letters(self) -> str:
+        """Letters that have been excluded by previous responses."""
         bad_letters = set()
 
         for guess, response in zip(self.guesses, self.responses):
@@ -46,7 +49,12 @@ class BasePlayer(PlayerInterface):
                 if letter_info == LetterInfo.NOT_IN_WORD:
                     bad_letters.add(letter)
 
-        return "".join(i for i in ascii_lowercase if i not in bad_letters)
+        return "".join(bad_letters)
+
+    @property
+    def remaining_letters(self) -> str:
+        """Letters that have not been excluded by previous responses."""
+        return "".join(i for i in ascii_lowercase if i not in self.bad_letters)
 
     @property
     def implied_regex(self) -> re.Pattern:
@@ -57,13 +65,20 @@ class BasePlayer(PlayerInterface):
                 if letter_and_info[1] == LetterInfo.IN_POSITION:
                     in_pos[idx] = letter_and_info[0]
 
-        pattern = "^" + "".join(in_pos.get(i, f"[{self.remaining_letters}]") for i in range(self.word_size)) + "$"
+        if not self.bad_letters:
+            pattern = ".*"
+        else:
+            pattern = "".join(in_pos.get(i, f"[^{self.bad_letters}]") for i in range(self.word_size))
         return re.compile(pattern)
 
     @property
     def remaining_words(self) -> list[str]:
         """The remaining words that conform to previous responses."""
-        return list(filter(self.implied_regex.fullmatch, self.all_words))
+        if self.guesses != self._cache_guesses:
+            self._cache_guesses = self.guesses.copy()
+            self._cache_remaining_words = list(filter(self.implied_regex.fullmatch, self._cache_remaining_words))
+
+        return self._cache_remaining_words
 
     @property
     def letter_frequencies(self) -> dict[str, int]:
